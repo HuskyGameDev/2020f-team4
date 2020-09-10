2233a75a9b375949ea803ab148cc2c983b1e6d61
Maritza Gonzalez <59942843+mcgonzale@users.noreply.github.com>
2020-09-10 19:29:30 -0400

    Test/Created Currency
    Just a test to make sure this be pushing correct.
    
    Created a script called Currency

---
+++ TDVR/Assets/Maritza/Scripts/Currency.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Currency : MonoBehaviour
+{
+    public int availableCurrency = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void IncrementCurrency()
+    {
+        availableCurrency++;
+    }
+
+    public void SpendCurrency(int cost)
+    {
+        availableCurrency = availableCurrency - cost;
+    }
+
+    public void IncreaseCurrency(int gain)
+    {
+        availableCurrency = availableCurrency + gain;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
