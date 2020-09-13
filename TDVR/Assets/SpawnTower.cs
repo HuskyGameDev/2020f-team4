31d458ba6817084dfb3029dca3a5d47f42da1c8b
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2020-09-12 22:50:18 -0400

    Basic VR Interactions
    Grab objects, teleport, snap sockets, spawn socket

---
+++ TDVR/Assets/SpawnTower.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SpawnTower : XRSocketInteractor
+{
+    public GameObject objectToSpawn;
+    public GameObject spawnedObject;
+    public Transform spawnedParent;
+    public float cooldownTime = 1;
+
+    private XRSocketInteractor socket;
+    private Transform trans;
+    private float lastPulledTime = 0;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+        trans = GetComponent<Transform>();
+        if (!spawnedObject)
+        {
+            spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+        }
+        if (!spawnedParent)
+        {
+            spawnedParent = trans;
+        }
+    }
+
+    new void OnTriggerExit(Collider interactable)
+    {
+
+        base.OnTriggerExit(interactable);
+        if (interactable.tag == "Tower")
+        {
+            spawnedObject = null;
+            if (Time.fixedTime - lastPulledTime > cooldownTime)
+            {
+                spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+                lastPulledTime = Time.fixedTime;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!spawnedObject)
+        {
+            if (Time.fixedTime - lastPulledTime > cooldownTime)
+            {
+                spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+                lastPulledTime = Time.fixedTime;
+            }
+        }
+    }
+}
---
+++ TDVR/Assets/TeleportController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    private XRRayInteractor rightRayInteractor;
+    private XRRayInteractor leftRayInteractor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (rightTeleportRay)
+            rightRayInteractor = rightTeleportRay.gameObject.GetComponent<XRRayInteractor>();
+        if (leftTeleportRay)
+            leftRayInteractor = leftTeleportRay.gameObject.GetComponent<XRRayInteractor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+            bool active = CheckIfActivated(leftTeleportRay);
+            leftRayInteractor.allowSelect = active;
+            leftTeleportRay.gameObject.SetActive(active);
+        }
+
+        if (rightTeleportRay)
+        {
+            bool active = CheckIfActivated(rightTeleportRay);
+            rightRayInteractor.allowSelect = active;
+            rightTeleportRay.gameObject.SetActive(active);
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+
+    public void SetRotation(Transform reference)
+    {
+        transform.SetPositionAndRotation(transform.position, Quaternion.Euler(0, reference.eulerAngles.y, 0));
+    }
+}
