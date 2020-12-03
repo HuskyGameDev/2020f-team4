cd56f93309238ce5ff94ce75d3465e0dec6a2579
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2020-12-03 17:50:22 -0500

    Stephen dev (#4)
    * Test/Created Currency
    
    Just a test to make sure this be pushing correct.
    
    Created a script called Currency
    
    * Basic VR Interactions
    
    Grab objects, teleport, snap sockets, spawn socket
    
    * More VR Setup
    
    Refined snap socket prefabs, a socket now follows the horizontal position of a tower when held and sits .1 above the ground
    
    * Raycast socket projection
    
    The location of the socket is now based on a spherecast straight down from the block.  If it hits anything other than "Placeable Ground", it doesn't update.  For some reason, the spherecasts don't stop when the block is put down.
    
    * Fixed snap locations & spawner
    
    Removed all use of built in XR socket because it causes interaction issues.  Built own, specialized version.
    
    * Currency System, Started Implementation of
    
    Created a class that handles incrementing, decreasing, etc. of currency used by the player to play the game.
    
    Created an object in-scene that displays current currency to user via TextMeshPro, and a script to handle the necessary updates of the display
    
    Co-Authored-By: Sawyer Knowles <46873273+SawyerKnowles@users.noreply.github.com>
    Co-Authored-By: Stephen Ogden <42942300+sjogden@users.noreply.github.com>
    Co-Authored-By: FrankVanHove <46799346+FrankVanHove@users.noreply.github.com>
    
    * Documentation
    
    Added documentation to custom scripts.  Also added ability to set allowed layer name on spherecast from editor
    
    * Integrated Currency Scripts to Tower Spawning
    
    Towers now only spawn if the currency is above a configurable amount.  Also minor bugfixes to remove console warnings.
    
    * Menu Scene Started
    
    A menu scene is created with a VR rig in it.  There is a menu pane with 3 buttons on it that I can select with a ray pointer from the controllers, but they do not have actions attached yet.
    
    * Added ability to assign actions to buttons
    
    Script ButtonActions can now be added to panes to allow the association of Buttons to Actions.  This is configurable through the inspector via the instructions in the comments.
    
    * Deleted problematic meta files
    
    Co-authored-by: Maritza Gonzalez <59942843+mcgonzale@users.noreply.github.com>
    Co-authored-by: Sawyer Knowles <46873273+SawyerKnowles@users.noreply.github.com>
    Co-authored-by: FrankVanHove <46799346+FrankVanHove@users.noreply.github.com>

---
+++ TDVR/Assets/Stephen/Scripts/SnapFollowObject.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SnapFollowObject : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public Transform spawner;
+    public bool showShpereCastInGizmo = false;
+    public string allowedLayer = "Placeable Ground";
+
+    private GameObject snapZone;
+    private bool exists = false;
+    private bool intersected = false;
+    private Transform trans;
+    private float currentHitDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        snapZone = transform.parent.GetChild(1).gameObject;
+    }
+
+    // CreateSnapZone creates a hovering "holographic" snap zone, called whenever the object is picked up
+    public void CreateSnapZone()
+    {
+        snapZone.GetComponentInChildren<MeshRenderer>().enabled = true;
+        snapZone.SetActive(true);
+        exists = true;
+    }
+
+    // DestroySnapZone deactivates the hovering "holographic" snap zone, called whenever the object is let go
+    public void DestroySnapZone()
+    {
+        // snap the object into position if it is in the snapzone
+        if (intersected)
+        {
+            transform.position = snapZone.transform.GetChild(0).position;
+            transform.localRotation = snapZone.transform.rotation;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+        snapZone.GetComponentInChildren<MeshRenderer>().enabled = false;
+        snapZone.SetActive(false);
+        exists = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // when object is held, perform spherecast directly down to see if the surface below the object is legal to place the object onto
+        if (exists)
+        {
+            if (Physics.SphereCast(trans.position, trans.localScale.x/2, Vector3.down, out RaycastHit hitInfo, Mathf.Infinity, layerMask) && hitInfo.transform.gameObject.layer == LayerMask.NameToLayer(allowedLayer))
+            {
+                currentHitDistance = hitInfo.distance;
+                snapZone.transform.position = hitInfo.point;
+                snapZone.GetComponent<Transform>().rotation = Quaternion.Euler(0, trans.rotation.eulerAngles.y, 0);
+                Debug.Log(GetInstanceID());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if it intersects with the snapzone, allow snapping upon drop
+        if (other != null && snapZone != null)
+            if (other.gameObject.GetInstanceID() == snapZone.transform.GetChild(0).gameObject.GetInstanceID())
+            {
+                intersected = true;
+            }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if it stops intersecting with the snapzone, disallow snapping upon drop
+        if (other != null && snapZone != null)
+            if (other.gameObject.GetInstanceID() == snapZone.transform.GetChild(0).gameObject.GetInstanceID())
+                intersected = false;
+    }
+
+    // necessary to display wireframe spherecast in editor
+    private void OnDrawGizmosSelected()
+    {
+        if (showShpereCastInGizmo)
+        {
+            Gizmos.color = Color.red;
+            Debug.DrawLine(trans.position, trans.position + Vector3.down * currentHitDistance);
+            Gizmos.DrawWireSphere(trans.position + Vector3.down * currentHitDistance, trans.localScale.x);
+        }
+    }
+}
---
+++ TDVR/Assets/Stephen/Scripts/SpawnTower.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SpawnTower : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public GameObject spawnedObject;
+    public Transform spawnedParent;
+    public Currency balance;
+    public float cooldownTime = 1;
+    public int buyCost = 100;
+    
+    private Transform trans;
+    private float lastPulledTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if object not already socketed, create object
+        trans = GetComponent<Transform>();
+        if (!spawnedObject)
+        {
+            //spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+        }
+        if (!spawnedParent)
+        {
+            spawnedParent = trans;
+        }
+    }
+
+    void OnTriggerExit(Collider interactable)
+    {
+        // when a tower is removed, reset spawn timer
+        if (interactable.tag == "Tower" && spawnedObject)
+        {
+            spawnedObject = null;
+            /*if (Time.fixedTime - lastPulledTime > cooldownTime)
+            {
+                spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+                lastPulledTime = Time.fixedTime;
+            }*/
+            // testing
+            lastPulledTime = Time.fixedTime;
+            balance.SpendCurrency(100);
+        }
+    }
+
+    private void Update()
+    {
+        // if an object is not spawned, check cooldown to see if it can spawn again
+        if (!spawnedObject)
+        {
+            if ((Time.fixedTime - lastPulledTime > cooldownTime) && (balance.availableCurrency >= buyCost))
+            {
+                spawnedObject = Instantiate(objectToSpawn, trans.position, trans.rotation, spawnedParent);
+                lastPulledTime = Time.fixedTime;
+            }
+        }
+    }
+}
