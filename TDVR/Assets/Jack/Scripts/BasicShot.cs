237f20df0d3c0708b0a7f3b3b4470284de79ffe4
Stephen Ogden <42942300+sjogden@users.noreply.github.com>
2020-12-05 14:45:41 -0500

    Jack main (#9)
    * Pathing Script finished
    
    * Implemented turret targeting and bullet firing scrips
    
    * Turret damge/ Enemy Health
    
    * Merge fix
    
    * folder fix
    
    * fix #3
    
    Co-authored-by: ZeptoHaze <zeptohaze@gmail.com>

---
+++ TDVR/Assets/Jack/Scripts/BasicShot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShot : MonoBehaviour
+{
+    [Header("Variables (WIP)")]
+    private Transform target;
+    public float speed = 100f;
+    public int damage = 1;
+
+    public void Attack (Transform targets)
+    {
+        target = targets;
+    }
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float traveled = speed * Time.deltaTime;
+
+        if(direction.magnitude <= traveled)
+        {
+            Hit();
+            return;
+        }
+
+        transform.Translate(direction.normalized * traveled, Space.World);
+
+    }
+
+    void Hit()
+    {
+        Destroy(gameObject);
+        Damage(target);
+    }
+
+    void Damage(Transform enemy)
+    {
+        EnemyStat e = enemy.GetComponent<EnemyStat>();
+
+        if ( e!= null)
+        {
+            e.TakeDamage(damage);
+        }
+      
+    }
+}
