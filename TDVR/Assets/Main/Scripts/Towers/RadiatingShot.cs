2ca648c2006607ad9f5969960d91aa3182be43d6
FrankVanHove <46799346+FrankVanHove@users.noreply.github.com>
2021-02-24 21:03:15 -0500

    AOE Turret
    Contains Assets, Scripts, and Prefabs all relating to the AOE turret

---
+++ TDVR/Assets/Main/Scripts/Towers/RadiatingShot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadiatingShot : BasicShot
+{
+    private float startTime;
+    public float deleteGap = 1f;
+
+    public override void Attack (Transform targets)
+    {
+        startTime = Time.time;
+    }
+
+    //Damage Enemies when they enter the Collider
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy"){
+            Debug.Log("Hitting Enemy with Collider");
+            base.Damage(other.transform);
+        }
+
+    }
+
+    //Timer for when to delete the collider
+    void Update()
+    {
+        if(Time.time - startTime >= deleteGap){
+            Destroy(gameObject);
+        }
+
+    }
+
+}
